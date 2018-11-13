d5991bdbcf8ed3a68df8c7d885b561d850180119
Gabriel Urbain <gabriel.urbain@ugent.be>
2018-11-13 15:10:11 +0100

    changing hack timeout

--- docker/install/hack_me.sh
+++ docker/install/hack_me.sh
@@ -2,10 +2,12 @@
 
 source /opt/ros/kinetic/setup.bash
 source /opt/ros/dls-distro/setup.bash
-
-
 sudo apt-get install -y coreutils
 
-timeout 25 /opt/ros/kinetic/bin/roslaunch \
+timeout 2m /opt/ros/kinetic/bin/roslaunch \
            dls_supervisor operator.launch \
            gazebo:=true osc:=false gui:=false rviz:=false
+RETVAL=$?
+
+echo "The Hack finished with the value= $RETVAL"
+exit 0
