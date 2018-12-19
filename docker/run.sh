3230d8ccd43a154681a965f319ebf7e6156fb018
Gabriel Urbain <gurbain@ugent.be>
2018-12-19 17:21:32 +0100

    Correct Dockerfile wheel and mount docker_sim folder in run

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+DATA_FOLDER="/home/gurbain/docker_sim"
+
 # docker run \
 #        --add-host=koe:172.18.20.19 \
 #        --add-host=koe.elis.ugent.be:172.18.20.19 \
@@ -16,11 +19,22 @@
 #        -e DISPLAY=$DISPLAY \
 #        -it hyq:latest /bin/bash
 
-
-docker run \
+if [ -d "$DATA_FOLDER" ]; then
+	docker run \
+       --add-host=paard:192.168.0.2 \
+       --add-host=geit:192.168.0.5 \
+       --add-host=kat:192.168.0.3 \
+       --add-host=schaap:192.168.0.1 \
+       -e DISPLAY=$DISPLAY \
+       -v $DATA_FOLDER:$DATA_FOLDER \
+       -it hyq:latest /bin/bash
+else
+	docker run \
        --add-host=paard:192.168.0.2 \
        --add-host=geit:192.168.0.5 \
        --add-host=kat:192.168.0.3 \
        --add-host=schaap:192.168.0.1 \
        -e DISPLAY=$DISPLAY \
        -it hyq:latest /bin/bash
+fi
+
