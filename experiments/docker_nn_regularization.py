e9cbac075cbd21d92d73b871f0863d12ee8a83af
Gabriel Urbain <gabrielurbain@gmail.com>
2019-01-09 15:02:25 +0100

    Save final metrics in simulation

---
+++ experiments/docker_nn_regularization.py
@@ -0,0 +1,31 @@
+
+import ConfigParser
+import numpy as np
+
+from hyq import cluster
+
+
+DEF_CONFIG = "/home/gurbain/hyq_ml/config/sim_config_nn_default.txt"
+
+
+if __name__ == '__main__':
+
+    # Create a docker task manager
+    task_manager = cluster.Tasks()
+
+    # Create a list of experiments
+    exp_list = []
+    for i in range(4):
+        for r in np.logspace(0, 0.1, 6):
+
+            # Open the config file and retrieve the data
+            config = ConfigParser.ConfigParser()
+            config.read(DEF_CONFIG)
+            config.set("Network", "regularization", str(r))
+
+            # Add it to the experiment list
+            exp_list.append(config)
+
+    # Process all in parallel
+    exp_res_dirs = task_manager.process(exp_list)
+    print exp_res_dirs
--- hyq/simulation.py
+++ hyq/simulation.py
@@ -9,12 +9,14 @@ import ConfigParser
 import copy
 from keras import backend as K
 import math
+#  import matplotlib.pyplot as plt
 import numpy as np
 import pexpect
 import pickle
 import psutil
 import rospy as ros
 import rosbag
+from scipy.interpolate import interp1d
 import sys
 import tensorflow as tf
 import threading
@@ -46,7 +48,6 @@ class Simulation(object):
         self.publish_states = None
         self.publish_loss = None
         self.publish_error = None
-        self.save_sim = False
         self.sim_file = None
         self.ol = None
         self.epoch_num = 0
@@ -60,15 +61,24 @@ class Simulation(object):
         self.init_impedance = None
         self.remote = False
         self.real_time = False
-        self.save_physics = False
-        self.save_physics_t_real = []
-        self.save_physics_x = []
-        self.save_physics_y = []
-        self.save_physics_z = []
-        self.save_physics_phi = []
-        self.save_physics_theta = []
-        self.save_physics_psi = []
-        self.save_physics_t_trot = 0
+
+        # Saving
+        self.save_ctrl = False
+        self.save_ctrl_state = []
+        self.save_ctrl_action = []
+        self.save_states = False
+        self.save_states_t_real = []
+        self.save_states_x = []
+        self.save_states_y = []
+        self.save_states_z = []
+        self.save_states_phi = []
+        self.save_states_theta = []
+        self.save_states_psi = []
+        self.save_states_t_trot = 0
+        self.save_metrics = False
+        self.save_stop_train_i = 0
+        self.save_start_test_i = 0
+        self.save_index = 0
 
         # Class uninitialized objects
         self.physics = None
@@ -92,8 +102,6 @@ class Simulation(object):
         # Training variable
         self.t_hist = []
         self.state = deque([])
-        self.state_history = []
-        self.action_history = []
         self.x_train_step = []
         self.y_train_step = []
         self.loss = 1.0
@@ -133,7 +141,7 @@ class Simulation(object):
         # Open the config file and retrieve the data
         config_parser = ConfigParser.ConfigParser()
         config_parser.read(self.folder + "/config.txt")
-        self.config = {s:dict(config_parser.items(s)) for s in config_parser.sections()}
+        self.config = {s: dict(config_parser.items(s)) for s in config_parser.sections()}
 
         self.t_sim = float(self.config["Timing"]["t_sim"])
         self.t_train = float(self.config["Timing"]["t_train"])
@@ -144,22 +152,27 @@ class Simulation(object):
         # Class variable
         self.plot = eval(self.config["Debug"]["plot"])
         self.verbose = int(self.config["Debug"]["verbose"])
+        self.view = eval(self.config["Debug"]["view"])
+
+        self.sm = eval(self.config["Simulation"]["sm"])
+        self.ol = eval(self.config["Simulation"]["ol"])
         self.publish_actions = eval(self.config["Simulation"]["pub_actions"])
         self.publish_states = eval(self.config["Simulation"]["pub_states"])
         self.publish_loss = eval(self.config["Simulation"]["pub_loss"])
         self.publish_error = eval(self.config["Simulation"]["pub_error"])
-        self.save_sim = eval(self.config["Simulation"]["save"])
-        self.sim_file = self.config["Network"]["sim_file"]
-        self.ol = eval(self.config["Simulation"]["ol"])
+        self.save_ctrl = eval(self.config["Simulation"]["save_ctrl"])
+        self.save_states = eval(self.config["Simulation"]["save_states"])
+        self.save_metrics = eval(self.config["Simulation"]["save_metrics"])
+
         self.epoch_num = int(self.config["Training"]["epoch_num"])
+        self.train = eval(self.config["Training"]["train"])
         self.train_buff_size = int(self.config["Training"]["train_buff_size"])
-        self.view = eval(self.config["Debug"]["view"])
+
         self.init_impedance = eval(self.config["Physics"]["init_impedance"])
         self.remote = eval(self.config["Physics"]["remote_server"])
         self.real_time = eval(self.config["Physics"]["real_time"])
-        self.save_physics = eval(self.config["Physics"]["save"])
-        self.sm = eval(self.config["Simulation"]["sm"])
-        self.train = eval(self.config["Training"]["train"])
+
+        self.sim_file = self.config["Network"]["sim_file"]
 
     def start(self):
 
@@ -201,8 +214,6 @@ class Simulation(object):
                                       checkpoint=eval(self.config["Network"]["checkpoint"]),
                                       no_callbacks=eval(self.config["Network"]["no_callbacks"]),
                                       random_state=int(self.config["Network"]["random_state"]))
-
-
 
             self.network.load_data(True)
 
@@ -370,7 +381,7 @@ class Simulation(object):
 
         # GET DATA (ALWAYS)
         curr_state, rec_state = self.get_states()
-        pred_action, tgt_action = self.get_actions(curr_state, pred=(self.train_it > 0)) #(self.train_sm_mode == "Running"))
+        pred_action, tgt_action = self.get_actions(curr_state, pred=(self.train_it > 0))
 
         # TRAINING MODE
         if self.train_sm_mode == "Training":
@@ -398,8 +409,8 @@ class Simulation(object):
                     if len(self.x_train_step) > self.train_buff_size:
                         # When training is finished start again with new buffer
                         if not self.training_thread.isAlive():
-                            x = np.mat(self.x_train_step) # [-self.train_buff_size:])
-                            y = np.mat(self.y_train_step) #[-self.train_buff_size:])
+                            x = np.mat(self.x_train_step)  # [-self.train_buff_size:])
+                            y = np.mat(self.y_train_step)  # [-self.train_buff_size:])
                             self.training_thread = threading.Thread(target=self._training_thread,
                                                                     args=[x, y])
                             # Hack to avoid racing condition between training and execution thread
@@ -451,9 +462,9 @@ class Simulation(object):
         self._publish_loss()
 
         # Save states and actions
-        if self.save_sim:
-            self.state_history.append(curr_state)
-            self.action_history.append(mix_action)
+        if self.save_ctrl:
+            self.save_ctrl_state.append(curr_state)
+            self.save_ctrl_action.append(mix_action)
 
     def train_step(self):
 
@@ -519,9 +530,9 @@ class Simulation(object):
         self._publish_loss()
 
         # Save states and actions
-        if self.save_sim:
-            self.state_history.append(curr_state)
-            self.action_history.append(mix_action)
+        if self.save_ctrl:
+            self.save_ctrl_state.append(curr_state)
+            self.save_ctrl_action.append(mix_action)
 
     def step(self):
 
@@ -551,9 +562,9 @@ class Simulation(object):
         self._publish_loss()
 
         # Save states and actions
-        if self.save_sim:
-            self.state_history.append(curr_state)
-            self.action_history.append(mix_action)
+        if self.save_ctrl:
+            self.save_ctrl_state.append(curr_state)
+            self.save_ctrl_action.append(mix_action)
 
     def run(self):
 
@@ -581,7 +592,7 @@ class Simulation(object):
                     trot_flag = self.physics.start_rcf_trot()
                     if trot_flag:
                         self.printv(" ===== Trotting Started =====\n")
-                        self.save_physics_t_trot = copy.copy(self.t)
+                        self.save_states_t_trot = copy.copy(self.t)
 
                 # Apply noise on the robot
                 # if not self.remote and trot_flag:
@@ -605,51 +616,145 @@ class Simulation(object):
                               "\tSim Time: " + "{:.2f}".format(self.t) + " s" +
                               "\tNN Freq: " + "{:.2f}".format(f) + " Hz" +
                               " / Weight: "+"{:.2f}".format(self.nn_weight) +
-                              "\tRobot dist: {:.2f} m".format(math.sqrt(sum([float(i)**2 for i in self.physics.get_hyq_x_y_z()]))))
+                              "\tRobot dist: {:.2f} m".format(math.sqrt(sum([float(i)**2
+                                                                             for i in self.physics.get_hyq_x_y_z()]))))
                         last_it = self.it
                         last_t = self.t
 
-                # Save physics results
+                # Save physics states and metrics
                 self.t_hist.append(self.t)
-                if self.save_physics:
+                if self.save_states or self.save_metrics:
                     curr_x, curr_y, curr_z = self.physics.get_hyq_x_y_z()
                     curr_phi, curr_theta, curr_psi = self.physics.get_hyq_phi_theta_psi()
-                    self.save_physics_t_real.append(time.time() - t_init)
-                    self.save_physics_x.append(curr_x)
-                    self.save_physics_y.append(curr_y)
-                    self.save_physics_z.append(curr_z)
-                    self.save_physics_phi.append(curr_phi)
-                    self.save_physics_theta.append(curr_theta)
-                    self.save_physics_psi.append(curr_psi)
+                    self.save_states_t_real.append(time.time() - t_init)
+                    self.save_states_x.append(curr_x)
+                    self.save_states_y.append(curr_y)
+                    self.save_states_z.append(curr_z)
+                    self.save_states_phi.append(curr_phi)
+                    self.save_states_theta.append(curr_theta)
+                    self.save_states_psi.append(curr_psi)
+                    self.save_index += 1
+                    if self.nn_weight > 0 and self.save_stop_train_i == 0:
+                        self.save_stop_train_i = self.save_index
+                    if self.nn_weight >= 1.0 and self.save_start_test_i == 0:
+                        self.save_start_test_i = self.save_index
 
                 self.it += 1
         
-        self.printv(" ===== Final robot distance: {:.2f} m =====\n".format(math.sqrt(sum([float(i)**2 for i in self.physics.get_hyq_x_y_z()]))))
+        self.printv(" ===== Final robot distance: "
+                    "{:.2f} m =====\n".format(math.sqrt(sum([float(i)**2 for i in self.physics.get_hyq_x_y_z()]))))
         self.stop()
 
     def stop(self):
 
-        # Save the simulation
-        if self.save_sim:
-            with open(self.folder + "/state_action.pkl", "wb") as f:
-                pickle.dump([np.mat(self.state_history), np.mat(self.action_history), np.array(self.t_hist)],
+        # Stop the physics thread
+        self.physics.stop()
+
+        # Save the states and actions (IOs of the controller)
+        if self.save_ctrl:
+            with open(self.folder + "/ctrl.pkl", "wb") as f:
+                pickle.dump([np.mat(self.save_ctrl_state), np.mat(self.save_ctrl_action), np.array(self.t_hist)],
                             f, protocol=2)
 
             if self.sim_file is not None and not self.ol:
                 self.network.save()
 
-        # Save the physics
-        if self.save_physics:
-            to_save = {"t_sim": self.t_hist, "t_real": self.save_physics_t_real, "t_trot": self.save_physics_t_trot,
-                       "x": self.save_physics_x, "y": self.save_physics_y, "z": self.save_physics_z, 
-                       "phi": self.save_physics_phi, "theta": self.save_physics_theta, "psi": self.save_physics_psi}
+        # Save the physics states
+        if self.save_states:
+            to_save = {"t_sim": self.t_hist, "t_real": self.save_states_t_real, "t_trot": self.save_states_t_trot,
+                       "x": self.save_states_x, "y": self.save_states_y, "z": self.save_states_z,
+                       "phi": self.save_states_phi, "theta": self.save_states_theta, "psi": self.save_states_psi}
 
-            pickle.dump(to_save, open(self.folder + "/physics.pkl", "wb"), protocol=2)
+            pickle.dump(to_save, open(self.folder + "/states.pkl", "wb"), protocol=2)
 
+        # Save the robot metrics
+        if self.save_metrics:
+            if self.t_train > 0 and not self.ol and self.t_start_cl < self.t_sim:
+                (r_f, r_train_fft, r_test_fft, r_rms) = self._compute_diff_fft_sig(self.t_hist, self.save_states_psi,
+                                                                                   self.save_stop_train_i,
+                                                                                   self.save_start_test_i)
+                (p_f, p_train_fft, p_test_fft, p_rms) = self._compute_diff_fft_sig(self.t_hist, self.save_states_phi,
+                                                                                   self.save_stop_train_i,
+                                                                                   self.save_start_test_i)
+                to_save = {"train_roll_range": max(self.save_states_phi[0:self.save_stop_train_i]) -
+                                               min(self.save_states_phi[0:self.save_stop_train_i]),
+                           "train_pitch_range": max(self.save_states_psi[0:self.save_stop_train_i]) -
+                                                min(self.save_states_psi[0:self.save_stop_train_i]),
+                           "train_x_range": self.save_states_x[self.save_stop_train_i] - self.save_states_x[0],
+                           "train_y_range": abs(self.save_states_y[self.save_stop_train_i] - self.save_states_y[0]),
+                           "cl_roll_range": max(self.save_states_phi[self.save_stop_train_i:self.save_start_test_i]) -
+                                            min(self.save_states_phi[self.save_stop_train_i:self.save_start_test_i]),
+                           "cl_pitch_range": max(self.save_states_psi[self.save_stop_train_i:self.save_start_test_i]) -
+                                             min(self.save_states_psi[self.save_stop_train_i:self.save_start_test_i]),
+                           "cl_x_range": self.save_states_x[self.save_start_test_i] - self.save_states_x[self.save_stop_train_i],
+                           "cl_y_range": abs(self.save_states_y[self.save_start_test_i] - self.save_states_y[self.save_stop_train_i]),
+                           "test_roll_range": max(self.save_states_phi[self.save_start_test_i:]) -
+                                              min(self.save_states_phi[self.save_start_test_i:]),
+                           "test_pitch_range": max(self.save_states_psi[self.save_start_test_i:]) -
+                                               min(self.save_states_psi[self.save_start_test_i:]),
+                           "test_x_range": self.save_states_x[-1] - self.save_states_x[self.save_start_test_i],
+                           "test_y_range": abs(self.save_states_y[-1] - self.save_states_y[self.save_start_test_i]),
+                           "pitch_fft_rms": p_rms, "roll_fft_rms": r_rms,
+                           "t_train": self.t_hist[self.save_stop_train_i] - self.t_hist[0],
+                           "t_cl": self.t_hist[self.save_start_test_i] - self.t_hist[self.save_stop_train_i],
+                           "t_test": self.t_hist[-1] - self.t_hist[self.save_start_test_i]
+                           }
+            else:
+                to_save = {"roll_range": max(self.save_states_phi) - min(self.save_states_phi),
+                           "pitch_range": max(self.save_states_psi) - min(self.save_states_psi),
+                           "x_range": self.save_states_x[-1] - self.save_states_x[0],
+                           "y_range": abs(self.save_states_y[-1] - self.save_states_y[0]),
+                           }
+
+            pickle.dump(to_save, open(self.folder + "/metrics.pkl", "wb"), protocol=2)
+
+        # Stop plotjuggler
         if self.plot:
             self._stop_plotter()
 
-        self.physics.stop()
+    def _compute_diff_fft_sig(self, t, sig, ind_stop_train, ind_start_test):
+
+        # Find and interpolate vectors before and after training
+        sig1_fn = interp1d(t[0:ind_stop_train], sig[0:ind_stop_train])
+        sig2_fn = interp1d(t[ind_start_test:], sig[ind_start_test:])
+        t_max = min(t[ind_stop_train] - t[0], t[-1] - t[ind_start_test])
+        t_1 = np.linspace(t[20], t[0] + t_max - 1, 10000)
+        t_2 = np.linspace(t[ind_start_test+20], t[ind_start_test] + t_max - 1, 10000)
+        sig1 = sig1_fn(t_1)
+        sig2 = sig2_fn(t_2)
+
+        # Compute the two FFTs
+        ts = t_1[1] - t_1[0]
+        fs = 1.0/ts
+        n = len(sig1)
+        k = np.arange(n)
+        period = n / fs
+        frq = k / period
+        frq = frq[range(n/2)]
+        sig1_fft = np.fft.fft(sig1) / n
+        sig1_fft = sig1_fft[range(n/2)]
+        sig2_fft = np.fft.fft(sig2) / n
+        sig2_fft = sig2_fft[range(n/2)]
+
+        f_max_eval = int(20 * period)  # 20 Hz
+        # plt.plot(frq[0:f_max_eval], abs(sig1_fft[0:f_max_eval]), label="RCF sig FFT")
+        # plt.plot(frq[0:f_max_eval], abs(sig2_fft[0:f_max_eval]), label="NN sig FFT")
+        # plt.xlabel('Freq (Hz)')
+        # plt.ylabel('|Y(freq)|')
+        # plt.legend()
+        # plt.show()
+
+        # Compute RMS difference
+        fft_rms_diff = 0
+        for (x, y) in zip(abs(sig1_fft[0:f_max_eval]), abs(sig2_fft[0:f_max_eval])):
+            fft_rms_diff += (x - y) ** 2
+
+        # print "The RMS difference between the two FFT is: " + str(fft_rms_diff)
+
+        return (frq[0:f_max_eval],
+                abs(sig1_fft[0:f_max_eval]),
+                abs(sig2_fft[0:f_max_eval]),
+                math.sqrt(fft_rms_diff / len(sig1_fft[0:f_max_eval])))
 
     def _start_plotter(self):
 
@@ -770,8 +875,8 @@ class CPGSimulation(Simulation):
                                          queue_size=50)
 
         # Create history arrays
-        self.state_history = []
-        self.action_history = []
+        self.save_ctrl_state = []
+        self.save_ctrl_action = []
 
     def step(self):
 
@@ -806,9 +911,9 @@ class CPGSimulation(Simulation):
             self._publish_actions(predicted, target, action)
 
             # Save states and actions
-            if self.save_sim:
-                self.state_history.append(state.tolist()[0])
-                self.action_history.append(action.tolist())
+            if self.save_ctrl:
+                self.save_ctrl_state.append(state.tolist()[0])
+                self.save_ctrl_action.append(action.tolist())
 
 
 if __name__ == '__main__':
