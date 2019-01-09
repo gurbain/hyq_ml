67f678d0e8fabe527ba44d5fbbf503add44c97e4
Gabriel Urbain <gurbain@ugent.be>
2019-01-09 14:22:27 +0100

    move timeout check in cluster.py and debug typos

--- hyq/utils.py
+++ hyq/utils.py
@@ -191,24 +191,6 @@ def make_keras_picklable():
     cls.__getstate__ = __getstate__
     cls.__setstate__ = __setstate__
 
-
-class Timeout():
-    """Timeout class using ALARM signal."""
-    class Timeout(Exception):
-        pass
- 
-    def __init__(self, sec):
-        self.sec = sec
- 
-    def __enter__(self):
-        signal.signal(signal.SIGALRM, self.raise_timeout)
-        signal.alarm(self.sec)
- 
-    def __exit__(self, *args):
-        signal.alarm(0)    # disable alarm
- 
-    def raise_timeout(self, *args):
-        raise Timeout.Timeout()
  
 def main():
     # Run block of code with timeouts
