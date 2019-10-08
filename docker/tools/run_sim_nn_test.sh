b927407202cd1bff66192d1fa8659d47f00a6f2b
Gabriel Urbain <gabrielurbain@gmail.com>
2019-10-08 16:01:31 +0200

    Adapt tools for docker

--- docker/tools/run_sim_nn_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm  hyq:latest /bin/bash -c 'source /opt/ros/dls-distro/setup.bash; \
-                                        cd /home/gurbain/hyq_ml/hyq; \
-                                        mkdir /home/gurbain/a; \
-                                        ln -s /home/gurbain/hyq_ml/config/sim_config_nn_default.txt \
-                                        /home/gurbain/a/config.txt; \
-                                        roscore & python simulation.py /home/gurbain/a'
-
